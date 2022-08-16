a5ab560aac833b096cf8b08a812381b2dad03fed
無常 <3142324836@qq.com>
2022-08-16 21:27:46 +0800

    Update project

--- projects/Project.cpp
+++ projects/Project.cpp
@@ -1,1 +1,22 @@
 #include "Project.h"
+
+namespace vocalshaper {
+	Project::Project(const juce::String& name, const juce::String& path)
+		:name(name),path(path)
+	{}
+
+	const juce::String& Project::getName() const
+	{
+		return this->name;
+	}
+
+	const juce::String& Project::getPath() const
+	{
+		return this->path;
+	}
+
+	bool Project::getSaved() const
+	{
+		return this->saved;
+	}
+}
--- projects/Project.h
+++ projects/Project.h
@@ -1,9 +1,31 @@
-#pragma once
+﻿#pragma once
 #include "../Macros.h"
+
+#include <JuceHeader.h>
 
 namespace vocalshaper {
 	class VSAPI Project
 	{
+	public:
+		Project() = delete;
+		Project(const juce::String& name, const juce::String& path);
+		~Project() = default;
 
+	public:
+		const juce::String& getName() const;
+		const juce::String& getPath() const;
+
+		bool getSaved() const;
+
+	private:
+		// name:项目文件名
+		// path:项目路径
+		const juce::String name;
+		const juce::String path;
+
+		// saved:已保存
+		bool saved = true;
+
+		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Project)
 	};
 }
